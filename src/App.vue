b0396772223f5fb904b8a0a02614edd1cdfc9720
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-08-11 22:05:49 +0300

    Added Sorting Buton + table

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
-<template> 
-<BaseCard>
-  <Sorting />
-  <Table />
-</BaseCard>
+<template>
+  <BaseCard>
+    <Sorting @generate-people="getPeople" :isPlaying="isPlaying" />
+    <Table :generatedPeople="people" @row-reorder="reorder" />
+  </BaseCard>
 </template>
 
 <script>
-import Table from './components/Main/Table.vue'
-import Sorting from './components/Main/Sorting.vue'
+import Table from "./components/Main/Table.vue";
+import Sorting from "./components/Main/Sorting.vue";
 
 export default {
   components: {
     Table,
-    Sorting
-  }
+    Sorting,
+  },
+  data() {
+    return {
+      people: [],
+      isSorted: false,
+      timeToStart: 3000,
+    };
+  },
+  methods: {
+    getPeople(people) {
+      this.people = people;
+    },
+    reorder(values) {
+      this.people = values;
+      // this.isArraySorted();
+      this.arraySortedOrNot(this.people, this.people.length);
+    },
+    arraySortedOrNot(arr, n) {
+      // Array has one or no element
+      if (n == 0 || n == 1) return true;
+
+      for (let i = 1; i < n; i++)
+        // Unsorted pair found
+        if (arr[i - 1].potatoes > arr[i].potatoes) return false;
+
+      // No unsorted pair found
+      return true;
+    },
+  },
 };
 </script>
 
 <style>
 :root {
   /* buttons */
-  --button-primary: #FF8D00;
+  --color-primary: #ff8d00;
+  --color-gray: #cccccc;
 }
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 * {
---
+++ src/components/Main/Table.vue
@@ -0,0 +1,30 @@
+<template>
+  <DataTable :value="generatedPeople" @row-reorder="onRowReorder">
+    <Column :rowReorder="true" headerStyle="width: 3em" />
+    <Column field="email" header="Email"></Column>
+    <Column field="name" header="Name"></Column>
+    <Column field="potatoes" header="Potatoes"></Column>
+  </DataTable>
+  <!-- <button @click="isArraySorted">Test</button>
+  <p>{{ isSorted }}</p> -->
+</template>
+
+<script>
+export default {
+  emits: ["row-reorder"],
+  props: ["generatedPeople"],
+  data() {
+    return {
+      people: [],
+    };
+  },
+  methods: {
+    onRowReorder(event) {
+      // console.log(event.value);
+      this.$emit("row-reorder", event.value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
